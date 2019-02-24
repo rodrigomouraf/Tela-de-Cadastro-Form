eec9b96ae4b514aa579f0bb8529f321eb194802d
Rodrigo Moura <rodrigomoura_rm@hotmail.com>
2019-02-23 22:57:21 -0300

    Versão 1.0

---
+++ CadastroClientes/FrmCadastroClientes.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace CadastroClientes
+{
+    partial class FrmCadastroClientes
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnAbrir = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Código";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefone";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(96, 22);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 4;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(96, 174);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(100, 20);
+            this.txtTelefone.TabIndex = 5;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(96, 97);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(96, 239);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(96, 285);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 8;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            this.btnGravar.Click += new System.EventHandler(this.btnGravar_Click);
+            // 
+            // btnAbrir
+            // 
+            this.btnAbrir.Location = new System.Drawing.Point(218, 285);
+            this.btnAbrir.Name = "btnAbrir";
+            this.btnAbrir.Size = new System.Drawing.Size(75, 23);
+            this.btnAbrir.TabIndex = 9;
+            this.btnAbrir.Text = "Abrir";
+            this.btnAbrir.UseMnemonic = false;
+            this.btnAbrir.UseVisualStyleBackColor = true;
+            this.btnAbrir.Click += new System.EventHandler(this.btnAbrir_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(331, 285);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // FrmCadastroClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 359);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnAbrir);
+            this.Controls.Add(this.btnGravar);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmCadastroClientes";
+            this.Text = "Cadastro de Clientes";
+            this.Load += new System.EventHandler(this.FrmCadastroClientes_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnAbrir;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ CadastroClientes/FrmCadastroClientes.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadastroClientes
+{
+    public partial class FrmCadastroClientes : Form
+    {
+        public FrmCadastroClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCadastroClientes_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btnGravar_Click(object sender, EventArgs e)
+        {
+            // cria dataset, que pode ser uma coleção de tabelas
+            DataSet dataset = new DataSet("Dados");
+            // cria a tabela
+            DataTable tabela = CriarEstruturaTabela();
+            // adiciona tabela ao dataset
+            dataset.Tables.Add(tabela);
+            // adiciona registros na tabela
+            // criar os registros
+            DataRow registro = tabela.NewRow();
+            registro["Codigo"] = txtCodigo.Text;
+            registro["Nome"] = txtNome.Text;
+            registro["Fone"] = txtTelefone.Text;
+            registro["Email"] = txtEmail.Text;
+            tabela.Rows.Add(registro);
+            // salvando o cliente em um arquivo xml
+            dataset.WriteXml(@".\cliente_" + txtCodigo.Text + ".xml");
+        }
+
+        private DataTable CriarEstruturaTabela()
+        {
+            DataTable tabela = new DataTable("Clientes");
+            //cria colunas para a tabela
+            tabela.Columns.Add(new DataColumn("Codigo"));
+            tabela.Columns.Add(new DataColumn("Nome"));
+            tabela.Columns.Add(new DataColumn("Fone"));
+            tabela.Columns.Add(new DataColumn("Email"));
+            return tabela;
+        }
+
+        private void btnAbrir_Click(object sender, EventArgs e)
+        {
+            // cria o dataset
+            DataSet dataset = new DataSet();
+            // le o dataset do disco 
+            dataset.ReadXml(@"./Cliente_" + txtCodigo.Text + ".xml");
+            // tabela é o primeiro datatable da coleção
+            DataTable tabela = dataset.Tables[0];
+            // considero o primeiro registro da tabela
+            DataRow registro = tabela.Rows[0];
+            // mostro dados do registro na tela
+            MostrarDadosTela(registro);
+
+        }
+
+        private void MostrarDadosTela(DataRow registro)
+        {
+            txtCodigo.Text = registro["Codigo"].ToString();
+            txtNome.Text = registro["Nome"].ToString();
+            txtTelefone.Text = registro["Fone"].ToString();
+            txtEmail.Text = registro["Email"].ToString();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            //percorrer todos os controles da tela para limpar
+            foreach (Control txt in Controls)
+                if (txt is TextBox)
+                    (txt as TextBox).Clear();
+                    
+        }
+    }
+}
